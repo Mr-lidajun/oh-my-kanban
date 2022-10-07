476f5d7196acf2e34cdf16afe64f647544b1cb66
atom <solidajun@163.com>
2022-10-07 11:53:30 +0800

    ch-03: 基础布局

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>我的看板</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="kanban-board">
+        <section className="kanban-column column-todo">
+          <h2>待处理</h2>
+        </section>
+        <section className="kanban-column column-ongoing">
+          <h2>进行中</h2>
+        </section>
+        <section className="kanban-column column-done">
+          <h2>已完成</h2>
+        </section>
+      </main>
     </div>
   );
 }
